2a6816a959febbb896e5b480aa75be282b88c6ff
bk <bkosciel@interia.pl>
2026-04-26 18:41:37 +0200

    5 czesc

--- src/main/java/com/koscielniak/projekt/controller/ProductController.java
+++ src/main/java/com/koscielniak/projekt/controller/ProductController.java
@@ -1,42 +1,35 @@
 package com.koscielniak.projekt.controller;
 
+
 import com.koscielniak.projekt.dto.Product;
-import com.koscielniak.projekt.dto.Promotion;
 import com.koscielniak.projekt.services.DataService;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import tools.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
-public class PromotionsController {
+public class ProductController {
 
     private final DataService dataService;
 
-    public PromotionsController(DataService dataService) {
+    public ProductController(DataService dataService) {
         this.dataService = dataService;
     }
 
-    @GetMapping("/promotions")
-    public List<Promotion> getAll() {
-        return dataService.getPromotions();
-    }
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return dataService.getProducts();
 
-    @GetMapping("/promotion/{id}")
-    public Promotion getOnePromo(@PathVariable String id) {
-        return dataService.getPromotions().stream()
+    }
+    @GetMapping("/product/{id}")
+    public Product getOneProduct(@PathVariable String id){
+        return dataService.getProducts().stream()
                 .filter(p -> p.id.equals(id))
                 .findFirst()
                 .orElse(null);
     }
-
-
-
 }
