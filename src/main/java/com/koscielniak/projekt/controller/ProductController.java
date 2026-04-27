ef0179a944b525c70de0ee3fd55ac758f86b5902
BK <bkosciel@interia.pl>
2026-04-27 13:38:58 +0200

    wyszukiwanie

--- src/main/java/com/koscielniak/projekt/controller/ProductController.java
+++ src/main/java/com/koscielniak/projekt/controller/ProductController.java
@@ -3,12 +3,12 @@ package com.koscielniak.projekt.controller;
 
 import com.koscielniak.projekt.dto.Product;
 import com.koscielniak.projekt.services.DataService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
@@ -20,16 +20,47 @@ public class ProductController {
         this.dataService = dataService;
     }
 
-    @GetMapping("/products")
+    @GetMapping("/productsAll")
     public List<Product> getAllProducts() {
         return dataService.getProducts();
 
     }
+
+    @GetMapping("/products")
+    public List<Product> getProducts(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false, name = "_sort") String sort,
+            @RequestParam(required = false, name = "_order") String order
+    ) {
+        List<Product> list = new java.util.ArrayList<>(dataService.getProducts().stream()
+                .filter(product -> product.name.contains(name) && product.category.contains(category))
+                .toList());
+        //Malejace
+        if (Objects.equals(sort, "price")){
+            list.stream()
+                    .sorted((a, b) -> a.getPrice().compareToIgnoreCase(b.getPrice()))
+                    .toList();
+        }
+        if (Objects.equals(sort, "name")){
+            list.stream()
+                    .sorted((a, b) -> a.getName().compareToIgnoreCase(b.getName()))
+                    .toList();
+        }
+
+        if (Objects.equals(order, "DESC")) {
+            Collections.reverse(list);
+        }
+
+        return list;
+    }
+
     @GetMapping("/product/{id}")
-    public Product getOneProduct(@PathVariable String id){
+    public Product getOneProduct(@PathVariable String id) {
         return dataService.getProducts().stream()
                 .filter(p -> p.id.equals(id))
                 .findFirst()
                 .orElse(null);
     }
+
 }
--- src/main/java/com/koscielniak/projekt/dto/Product.java
+++ src/main/java/com/koscielniak/projekt/dto/Product.java
@@ -1,8 +1,10 @@
 package com.koscielniak.projekt.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
 public class Product {
     public String id;
     public String name;
