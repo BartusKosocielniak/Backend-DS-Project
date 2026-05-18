b179103b84b6d1b5c8d35fededcb34a270839d4b
BK <bkosciel@interia.pl>
2026-05-18 13:38:12 +0200

    fixes in search

--- src/main/java/com/koscielniak/projekt/controller/ProductController.java
+++ src/main/java/com/koscielniak/projekt/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.koscielniak.projekt.dto.Product;
 import com.koscielniak.projekt.services.DataService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
@@ -33,22 +30,30 @@ public class ProductController {
             @RequestParam(required = false, name = "_sort") String sort,
             @RequestParam(required = false, name = "_order") String order
     ) {
-        List<Product> list = new java.util.ArrayList<>(dataService.getProducts().stream()
-                .filter(product -> product.name.contains(name) && product.category.contains(category))
-                .toList());
-        //Malejace
-        if (Objects.equals(sort, "price")){
+        List<Product> list = dataService.getProducts();
+        if (name!=null && !name.isEmpty()) {
+            list = new java.util.ArrayList<>(list.stream()
+                    .filter(product -> product.getName().equals(name))
+                    .toList());
+        }
+        if(category !=null && !category.isEmpty()){
+           list = new java.util.ArrayList<>(list.stream()
+                    .filter(product -> product.getCategory().equals(category))
+                    .toList());
+        }
+//        //Malejace
+        if (Objects.equals(sort, "price")) {
             list.stream()
                     .sorted((a, b) -> a.getPrice().compareToIgnoreCase(b.getPrice()))
                     .toList();
         }
-        if (Objects.equals(sort, "name")){
+        if (Objects.equals(sort, "name")) {
             list.stream()
                     .sorted((a, b) -> a.getName().compareToIgnoreCase(b.getName()))
                     .toList();
         }
 
-        if (Objects.equals(order, "DESC")) {
+        if (Objects.equals(order, "DESC") || Objects.equals(order, "desc")) {
             Collections.reverse(list);
         }
 
